1a17bcef58f4e95d690df9e169f3c02235098746
Pine Mizune <pinemz@gmail.com>
2015-09-15 20:44:56 +0900

    Fix webserver

--- src/web_server.cr
+++ src/web_server.cr
@@ -5,10 +5,32 @@ unless ENV.has_key? "PORT"
   exit 1
 end
 
+unless ENV.has_key? "HOST"
+  puts "`HOST` enviroment variable required!"
+  exit 1
+end
+
+module HTTP
+  class Server
+    def initialize(@host, @port, &@handler : Request -> Response)
+    end
+
+    def listen
+      @host.try do |host|
+        server = TCPServer.new(host, @port)
+        until @wants_close
+          spawn handle_client(server.accept)
+        end
+      end
+    end
+  end
+end
+
+host = ENV["HOST"]
 port = ENV["PORT"].to_i
-server = HTTP::Server.new(port) do |req|
+server = HTTP::Server.new(host, port) do |req|
   HTTP::Response.ok "text/plain", "OK"
 end
 
-puts "Listening on http://0.0.0.0:#{port}"
+puts "Listening on http://#{host}:#{port}"
 server.listen
