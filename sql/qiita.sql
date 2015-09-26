75abde39e09a2c01f5d88c951fdf3c2ecaa381b8
Pine Mizune <pinemz@gmail.com>
2015-09-27 06:59:13 +0900

    Implement Qiita bot

--- sql/qiita.sql
+++ sql/qiita.sql
@@ -1,6 +1,9 @@
-CREATE TABLE IF NOT EXISTS `qiita_item` (
+CREATE TABLE `qiita_item` (
   `id`        INTEGER UNSIGNED AUTO_INCREMENT,
   `item_id`   VARCHAR(255)     NOT NULL,
   `notice_id` VARCHAR(255)     NOT NULL,
   PRIMARY KEY (`id`)
 );
+
+CREATE UNIQUE INDEX `qiita_item_item_id_notice_id_uniq_idx`
+  ON `qiita_item` (`item_id`, `notice_id`);
