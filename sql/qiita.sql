95ae8fe45f56ce68b98503d09c8fb1c12141d55e
Pine Mizune <pinemz@gmail.com>
2015-09-27 07:28:19 +0900

    Revert "Merge pull request #3 from pine613/feature/qiita"
    This reverts commit 81eb9eacaf49f65c45bacf9e7a57c2d7f4a0fff4, reversing
    changes made to 637e051d3e7ac70cd721f77b5a49647caeb71d84.

--- sql/qiita.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `qiita_item` (
-  `id`        INTEGER UNSIGNED AUTO_INCREMENT,
-  `item_id`   VARCHAR(255)     NOT NULL,
-  `notice_id` VARCHAR(255)     NOT NULL,
-  PRIMARY KEY (`id`)
-);
-
-CREATE UNIQUE INDEX `qiita_item_item_id_notice_id_uniq_idx`
-  ON `qiita_item` (`item_id`, `notice_id`);
